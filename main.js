5b00987c4b514969c78e7356f86f85f3eb8920f7
gfernan24373 <gilberto_fernandez_100@hotmail.com>
2022-06-08 13:53:43 -0400

    dynamic properties

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// ------------------ Navbar Toogle ------------------
+
 const navOptions = document.querySelector('#nav-opc');
 const dropdownIcon = document.querySelector('#dropdown-icon');
 const navbar = document.querySelector('.navbar');
@@ -10,3 +12,141 @@ dropdownIcon.addEventListener('click', () => {
 	} 
 	
 });
+
+// ------------------ skills ------------------
+
+const html = document.querySelector('#html');
+const htmlSkill = document.querySelector('#html-skills');
+const js = document.querySelector('#javascript');
+const jsSkill = document.querySelector('#javascript-skills');
+const python = document.querySelector('#python');
+const pythonSkill = document.querySelector('#python-skills');
+const sql = document.querySelector('#sql');
+const sqlSkill = document.querySelector('#sql-skills');
+const certificate = document.querySelector('#certificates');
+const certificateImg = document.querySelector('#certificates-images');
+
+
+html.onclick = () => {
+	html.classList.add('add-white');
+	js.classList.remove('add-white');
+	python.classList.remove('add-white');
+	sql.classList.remove('add-white');
+	certificate.classList.remove('add-white');
+	htmlSkill.classList.remove('hide');
+	htmlSkill.classList.add('skill-content', 'animated');
+	jsSkill.classList.remove('skill-content');
+	jsSkill.classList.add('hide');
+	pythonSkill.classList.remove('skill-content');
+	pythonSkill.classList.add('hide');
+	sqlSkill.classList.remove('skill-content');
+	sqlSkill.classList.add('hide');
+	certificateImg.classList.remove('skill-content');
+	certificateImg.classList.add('hide');
+}
+
+js.onclick = () => {
+	js.classList.add('add-white');
+	html.classList.remove('add-white');
+	python.classList.remove('add-white');
+	sql.classList.remove('add-white');
+	certificate.classList.remove('add-white');
+	jsSkill.classList.remove('hide');
+	jsSkill.classList.add('skill-content', 'animated');
+	htmlSkill.classList.remove('skill-content');
+	htmlSkill.classList.add('hide');
+	pythonSkill.classList.remove('skill-content');
+	pythonSkill.classList.add('hide');
+	sqlSkill.classList.remove('skill-content');
+	sqlSkill.classList.add('hide');
+	certificateImg.classList.remove('skill-content');
+	certificateImg.classList.add('hide');
+}
+
+python.onclick = () => {
+	python.classList.add('add-white');
+	html.classList.remove('add-white');
+	js.classList.remove('add-white');
+	sql.classList.remove('add-white');
+	certificate.classList.remove('add-white');
+	pythonSkill.classList.remove('hide');
+	pythonSkill.classList.add('skill-content', 'animated');
+	htmlSkill.classList.remove('skill-content');
+	htmlSkill.classList.add('hide');
+	jsSkill.classList.remove('skill-content');
+	jsSkill.classList.add('hide');
+	sqlSkill.classList.remove('skill-content');
+	sqlSkill.classList.add('hide');
+	certificateImg.classList.remove('skill-content');
+	certificateImg.classList.add('hide');
+}
+
+sql.onclick = () => {
+	sql.classList.add('add-white');
+	html.classList.remove('add-white');
+	python.classList.remove('add-white');
+	html.classList.remove('add-white');
+	certificate.classList.remove('add-white');
+	sqlSkill.classList.remove('hide');
+	sqlSkill.classList.add('skill-content', 'animated');
+	htmlSkill.classList.remove('skill-content');
+	htmlSkill.classList.add('hide');
+	pythonSkill.classList.remove('skill-content');
+	pythonSkill.classList.add('hide');
+	jsSkill.classList.remove('skill-content');
+	jsSkill.classList.add('hide');
+	certificateImg.classList.remove('skill-content');
+	certificateImg.classList.add('hide');
+}
+
+certificate.onclick = () => {
+	certificate.classList.add('add-white');
+	html.classList.remove('add-white');
+	js.classList.remove('add-white');
+	python.classList.remove('add-white');
+	sql.classList.remove('add-white');
+	certificateImg.classList.remove('hide');
+	certificateImg.classList.add('skill-content', 'animated');
+	htmlSkill.classList.remove('skill-content');
+	htmlSkill.classList.add('hide');
+	jsSkill.classList.remove('skill-content');
+	jsSkill.classList.add('hide');
+	pythonSkill.classList.remove('skill-content');
+	pythonSkill.classList.add('hide');
+	sqlSkill.classList.remove('skill-content');
+	sqlSkill.classList.add('hide');
+}
+
+// html.onclick = () => {
+// 	htmlSkill.innerHTML = `
+// 				<p>- Strong Knowledges structuring and organizing HTML content.</p>
+// 				<p>- Knowledges about file organization, specificity, selectors, custom properties, media queries, box model, positioning and stacking elements.</p>
+// 				<p>- Flexbox and Grid understanding.</p>
+// 				<p>- Basic transitions and animations.</p>
+// 				<p>- Fundamentals about CSS Frameworks like Bootstrap, Tailwind and Bulma.</p>
+// 				<p>- Create Accordion, Carousel, Card, Modal, Progress Bar, Navbar, Container, icons, images, and other components with pure CSS and Bootstrap.</p>`;
+// }
+
+// js.onclick = () => {
+// 	htmlSkill.innerHTML = `
+// 				<p>- Understanding about identifiers: variables, keywords, properties and functions. Types, values and variables.</p>
+// 				<p>- ES6 Fundamentals (Arrow function, promise, const and let, template String and spread operator).</p>
+// 				<p>- Web Browser: scripting document, scripting CSS, events handlers, Web Storage and Networking.</p>
+// 				<p>- Fundamentals about NodeJS and Express.</p>`;
+// }
+
+// python.onclick = () => {
+// 	htmlSkill.innerHTML = `
+// 				<p>- Understanding about identifiers: variables, keywords, properties and functions. Types, values and variables.</p>
+// 				<p>- Regular expression, HTTP and BeautifulSoup for web scrapping.</p>
+// 				<p>- Connecting, creating, inserting, modifying and deleting tables in a DB.</p>
+// 				<p>- Experimental knowledges with flask a Django.</p>`;
+// }
+
+// sql.onclick = () => {
+// 	htmlSkill.innerHTML = `
+// 				<p>- Create Relational DB with SQLite.</p>
+// 				<p>- Understanding SQL Schema means.</p>
+// 				<p>- Connecting, creating, inserting, modifying and deleting tables in a DB.</p>
+// 				<p>- Fundamentals Statements like CREATE,INSERT, UPDATE, DELETE or ALTER.</p>`;
+// }
